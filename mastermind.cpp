2a64a9d515c459bb162f6c47f009e0521ddba7ec
Henry Meer <102127883+HenryAzurduy@users.noreply.github.com>
2022-03-22 18:48:58 -0400

    Add files via upload

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,62 +1,54 @@
 #include <iostream>
+#include <stdlib.h>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 class Mastermind{
     public:
-    void matriz(int a,int b,int c,int d,int e){
-        cout<<"Ingrese el codigo de 5 digitos con 1 espacio entre cada uno."<<endl;
-        cin>>a;cin>>b;cin>>c;cin>>d;cin>>e;
-        char juez[5]={a,b,c,d,e},
-	clear;
-	int v,w,x,y,z;
-        cout<<"Ingrese el codigo de 5 digitos con 1 espacio entre cada uno."<<endl;
-        cin>>v;cin>>w;cin>>x;cin>>y;cin>>z;
-        char jugador[5]={v,w,x,y,z},
-	int i;
+	
+    int vec1[5];
+	int vec2[5];
+    void matriz(){
+	srand(time(NULL));
+        cout<<"Se ha generado el codigo de 5 digitos."<<endl;
 	for (int i=0;i<5;i++){
-        cout<<jugador[i]<<end;
+	cin.get();
+	vec1[i]= (rand()%10);
+	cout<<vec1[i]<<endl;
+	}
+        cout<<"Ingrese el codigo de 5 digitos con 1 espacio entre cada uno."<<endl;
+	for(int j=0;j<5;j++){	
+	int aux;
+	cin>>vec2[j];
+//	vec2[j]=aux;
+//aux= -1;
 }
-if(a==v){
-cout<<"Es correcto"<<endl;}
-else{cout<<"Es incorrecto"<<endl;
 }
-if(b==w){
-cout<<"Es correcto"<<endl;}
-else{cout<<"Es incorrecto"<<endl;
-}
-if(c==x){ 
-cout<<"Es correcto"<<endl;}
-else{cout<<"Es incorrecto"<<endl;
-}
-if(d==y){ 
-cout<<"Es correcto"<<endl;}
-else{cout<<"Es incorrecto"<<endl;
-}
-if(e==z){ 
-cout<<"Es correcto"<<endl;}
-else{cout<<"Es incorrecto"<<endl;
-}
-if(v==b or v==c or v==d or v==e){
-cout<<"Correcto pero en un lugar erroneo"<<endl;
-}
-if(w==a or w==c or w==d or w==e){ 
-cout<<"Correcto pero en un lugar erroneo"<<endl;
-}
-if(x==a or x==b or x==d or x==e){ 
-cout<<"Correcto pero en un lugar erroneo"<<endl;
-}
-if(y==a or y==b or y==c or y==e){ 
-cout<<"Correcto pero en un lugar erroneo"<<endl;
-}
-if(z==a or z==b or z==c or z==d){ 
-cout<<"Correcto pero en un lugar erroneo"<<endl;
-}
+	void comparar(){
+	for(int i=0;i<5;i++){
+	  for(int j=0;j<5;j++){
+			cout<<"el numero"<<vec2[i]<<endl;
+
+		if(vec1[i]==vec2[j]){
+
+			if(i==j){
+				cout<<i<<"Es la posicion correcta"<<endl;
+		}	else{
+				cout<<i<<"No es la posicion correcta"<<endl;}
+			}
+		else{
+			cout<<j<<"No es correcto"<<endl;}
+		}
+	   }
+	}
+	
 };
 int main(){
 Mastermind my_juego;
-int a,b,c,d,e;
-cout<<"BIENVENIDO A MASTERMIND"<<endl<<"Las instrucciones son las siguientes:"<<endl;
-cout<<"1. Se generara un codigo de 4 numeros del 1 al 9."<<endl<<"2. Usted tendra 10 oportunidades para adivirarlo."<<endl;
+cout<<"BIENVENIDO A MASTERMIND CON MATRICES"<<endl<<"Las instrucciones son las siguientes:"<<endl;
+cout<<"1. Se generara un codigo de 5 numeros del 1 al 9."<<endl<<"2. Usted tendra 10 oportunidades para adivirarlo."<<endl;
 cout<<"3. Se corregira cada digito independientemente"<<endl;
-my_juego.matriz(a,b,c,d,e,v,w,x,y,z);
+my_juego.matriz();
+my_juego.comparar();
 return 0;
 }
