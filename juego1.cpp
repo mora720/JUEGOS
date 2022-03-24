88c5933fd526dab7c015ebeee8c726d4733bb6aa
ezequiel101001 <102003033+ezequiel101001@users.noreply.github.com>
2022-03-23 22:58:00 -0400

    Update juego1.cpp

--- juego1.cpp
+++ juego1.cpp
@@ -7,7 +7,8 @@ class Mastermind{
     public:
 	
     int vec1[5];
-	int vec2[5];
+    int vec2[5];
+    int c=0;
     void matriz(){
 	srand(time(NULL));
         cout<<"Se ha generado el codigo de 5 digitos."<<endl;
@@ -16,36 +17,15 @@ class Mastermind{
 	vec1[i]= (rand()%10);
 	cout<<vec1[i]<<endl;
 	}
-        cout<<"Ingrese el codigo de 5 digitos con 1 espacio entre cada uno."<<endl;
-	for(int j=0;j<5;j++){	
-	int aux;
-	cin>>vec2[j];
-}
 }
 	void comparar(){
-	for(int i=0;i<5;i++){
-	  for(int j=0;j<5;j++){
-			cout<<"El digito "<<vec2[i]<<" "<<endl;
 
-		if(vec1[i]==vec2[j]){
-
-			if(i==j){
-				cout<<"En la posicion "<<i<<" es correcto"<<endl;
-		}	else{
-				cout<<"En la posicion "<<i<<"  no esta la posicion correcta"<<endl;}
-			}
-		else{
-			cout<<"En la posicion "<<j<<" no es correcto"<<endl;}
-		}
-	cout<<".................................................................................................................."<<endl;
-	   }
-	int c=0;
-	while(c<=5){
-	cout<<"Ingrese nuevamente"<<endl;
+	while(c<=3){
+	cout<<"Ingrese el codigo de 5 digitos con 1 espacio entre cada uno."<<endl;
 	for(int k=0;k<5;k++){
 		cin>>vec2[k];
 	}
-	 for(int i=0;i<5;i++){
+	for(int i=0;i<5;i++){
           for(int j=0;j<5;j++){
                         cout<<"El digito "<<vec2[i]<<" "<<endl;
 
@@ -59,21 +39,21 @@ class Mastermind{
                 else{
                         cout<<"En la posicion "<<j<<" no es correcto"<<endl;}
                 }
-
-	 }
         cout<<".................................................................................................................."<<endl;
-        }
-/*	if(vec2[1]==vec1[1] and vec2[2]==vec1[2] and vec2[3]==vec1[3] and vec2[4]==vec1[4] and vec2[5]==vec1[5]){
+           }
+	if(vec2[0]==vec1[0] and vec2[1]==vec1[1] and vec2[2]==vec1[2] and vec2[3]==vec1[3] and vec2[4]==vec1[4]){
 		cout<<"Felidades usted gano"<<endl;
+		break;
+		c=3;
 	}
-	else if(c==5){
-		if(vec1[1]!=vec2[1] or vec1[2]!=vec2[2] or vec1[3]!=vec2[3] or vec1[4]!=vec2[4] or vec1[5]!=vec2[5]){
+	else if(c==3){
+		if(vec1[0]!=vec2[0] or vec1[1]!=vec2[1] or vec1[2]!=vec2[2] or vec1[3]!=vec2[3] or vec1[4]!=vec2[4]){
 			cout<<"Usted perdio"<<endl;
 		}
-*/
-//	}
+
+	}
 	c++;
-	
+	}
 	}
 	
 };
