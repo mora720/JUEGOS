ad7beb1db74b5f3a6d8620966056ec63eda6aaa9
Henry Meer <102127883+HenryAzurduy@users.noreply.github.com>
2022-03-23 23:26:22 -0400

    Add files via upload

--- mastermind.cpp
+++ mastermind.cpp
@@ -7,49 +7,30 @@ class Mastermind{
     public:
 	
     int vec1[5];
-	int vec2[5];
+    int vec2[5];
+    int c=0;
     void matriz(){
 	srand(time(NULL));
-        cout<<"Se ha generado el codigo de 5 digitos."<<endl;
+        cout<<"Precione enter 5 veces para generar los 5 digitos random."<<endl;
 	for (int i=0;i<5;i++){
 	cin.get();
 	vec1[i]= (rand()%10);
-	cout<<vec1[i]<<endl;
+	cout<<"*"<<endl;
+//	cout<<vec1[i]<<endl;
 	}
-        cout<<"Ingrese el codigo de 5 digitos con 1 espacio entre cada uno."<<endl;
-	for(int j=0;j<5;j++){	
-	int aux;
-	cin>>vec2[j];
-}
 }
 	void comparar(){
-	for(int i=0;i<5;i++){
-	  for(int j=0;j<5;j++){
-			cout<<"El digito "<<vec2[i]<<" "<<endl;
 
-		if(vec1[i]==vec2[j]){
-
-			if(i==j){
-				cout<<"En la posicion "<<i<<" es correcto"<<endl;
-		}	else{
-				cout<<"En la posicion "<<i<<"  no esta la posicion correcta"<<endl;}
-			}
-		else{
-			cout<<"En la posicion "<<j<<" no es correcto"<<endl;}
-		}
-	cout<<".................................................................................................................."<<endl;
-	   }
+	while(c<=4){
+	cout<<"Ingrese el codigo de 5 digitos con 1 espacio entre cada uno."<<endl;
 	for(int k=0;k<5;k++){
-	cout<<"Ingrese nuevamente"<<endl;
-	int aux[k];
-	cin>>aux[k];
-	aux[k]=vec2[k];
-
-	 for(int i=0;i<5;i++){
+		cin>>vec2[k];
+	}
+	for(int i=0;i<5;i++){
           for(int j=0;j<5;j++){
                         cout<<"El digito "<<vec2[i]<<" "<<endl;
 
-                if(vec1[i]==aux[j]){
+                if(vec1[i]==vec2[j]){
 
                         if(i==j){
                                 cout<<"En la posicion "<<i<<" es correcto"<<endl;
@@ -61,16 +42,26 @@ class Mastermind{
                 }
         cout<<".................................................................................................................."<<endl;
            }
+	if(vec2[0]==vec1[0] and vec2[1]==vec1[1] and vec2[2]==vec1[2] and vec2[3]==vec1[3] and vec2[4]==vec1[4]){
+		cout<<"Felidades usted gano"<<endl;
+		break;
+		c=4;
+	}
+	else if(c==4){
+		if(vec1[0]!=vec2[0] or vec1[1]!=vec2[1] or vec1[2]!=vec2[2] or vec1[3]!=vec2[3] or vec1[4]!=vec2[4]){
+			cout<<"Usted perdio"<<endl;
+		}
 
 	}
-
+	c++;
+	}
 	}
 	
 };
 int main(){
 Mastermind my_juego;
 cout<<"BIENVENIDO A MASTERMIND CON MATRICES"<<endl<<"Las instrucciones son las siguientes:"<<endl;
-cout<<"1. Se generara un codigo de 5 numeros del 1 al 9."<<endl<<"2. Usted tendra 5 oportunidades para adivirarlo."<<endl;
+cout<<"1. Se generara un codigo de 5 numeros del 0 al 9."<<endl<<"2. Usted tendra 5 oportunidades para adivirarlo."<<endl;
 cout<<"3. Se corregira cada digito con el uso de posiciones de matrices independientemente"<<endl;
 my_juego.matriz();
 my_juego.comparar();
