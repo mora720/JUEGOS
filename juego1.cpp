862395531b240aea29197632c75c96fb8db8df74
Henry Meer <102127883+HenryAzurduy@users.noreply.github.com>
2022-03-23 23:24:08 -0400

    Update juego1.cpp

--- juego1.cpp
+++ juego1.cpp
@@ -60,7 +60,7 @@ class Mastermind{
 int main(){
 Mastermind my_juego;
 cout<<"BIENVENIDO A MASTERMIND CON MATRICES"<<endl<<"Las instrucciones son las siguientes:"<<endl;
-cout<<"1. Se generara un codigo de 5 numeros del 1 al 9."<<endl<<"2. Usted tendra 5 oportunidades para adivirarlo."<<endl;
+cout<<"1. Se generara un codigo de 5 numeros del 0 al 9."<<endl<<"2. Usted tendra 5 oportunidades para adivirarlo."<<endl;
 cout<<"3. Se corregira cada digito con el uso de posiciones de matrices independientemente"<<endl;
 my_juego.matriz();
 my_juego.comparar();
